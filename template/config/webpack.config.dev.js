440023ac2f70d28480f96c83f4e2cecae688408b
undead25 <651182414@qq.com>
2017-11-04 22:38:01 +0800

    prepre config

---
+++ template/config/webpack.config.dev.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const autoprefixer = require('autoprefixer');
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+module.exports = {
+  devtool: 'cheap-module-source-map',
+}
