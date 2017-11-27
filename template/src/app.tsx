f12a3411eb2f1052ded05cdd966dcb3d5d69988f
undead25 <651182414@qq.com>
2017-11-28 00:18:22 +0800

    use glob in meta and add ts contents

---
+++ template/src/app.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+
+import logo from './logo.svg';
+{{#if sass}}
+import './app.scss';
+{{else if less}}
+import './app.less';
+{{else}}
+import './app.css';
+{{/if}}
+
+const App = () => (
+  <div className="app">
+    <header className="app-header">
+      <img src={logo} alt="logo" className="app-logo" />
+      <h1 className="app-title">Welcome to React</h1>
+    </header>
+    <p className="app-intro">
+      To get started, edit <code>src/app.js</code> and save to reload.
+    </p>
+  </div>
+);
+
+export default App;
